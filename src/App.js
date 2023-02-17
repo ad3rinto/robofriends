e40a6daaf861598e543d3ad880d49e3883bdae73
ad3rinto <aad3rinto@gmail.com>
2023-02-17 16:31:49 +0000

    remove redundant files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import "tachyons";
+import Card from './Card';
 
 
 function App() {
   return (
     <div className="App tc f1">
-      <h1>Mojoyin Aderinto</h1>
-      <h6>Naughty Child</h6>
+      <Card />
     </div>
   );
 }
