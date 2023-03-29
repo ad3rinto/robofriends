cc937015cd0be79a7452fbd6b7f92a0118a2e9e2
Micheal A <46092157+ad3rinto@users.noreply.github.com>
2023-03-29 19:18:24 +0100

    not sure
    Refactoring

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Argentina Apollo</h1>
+      <h1>Argentina Apollo 1</h1>
       <h6>Might Ighor</h6>
     </div>
   );
